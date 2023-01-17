947580044a322c1f12edbd7e65270f1f3558f38e
SandraFGG <sandraf.gonzalezgar@gmail.com>
2023-01-16 19:35:01 -0600

    se agrega la funcion suma

---
+++ operaciones.py
@@ -0,0 +1,10 @@
+def suma():
+    print('Elegiste la opción suma')
+    primerNumero = int(input('Proporciona el primer número: '))
+    segundoNumero = int(input('Proporciona el segundo número: '))
+    print(f'{primerNumero} + {segundoNumero} = {primerNumero + segundoNumero}')
+
+
+def salir():
+    print('hasta nunca')
+
